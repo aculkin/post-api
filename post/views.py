e4335d914cb725203a0eaaea2bd6721d3072bb10
Andrew Culkin <andrewsculkin@gmail.com>
2021-10-26 21:03:21 -0400

    Add group view sets

--- post/views.py
+++ post/views.py
@@ -1,10 +1,10 @@
-from post.serializers import PostSerializer
+from post.serializers import PostSerializer, GroupListSerializer
 
-from rest_framework import viewsets, mixins, status
+from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Post
+from core.models import Post, Group
 
 class PostListViewset(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.RetrieveModelMixin, mixins.ListModelMixin, mixins.DestroyModelMixin):
     """Manage the posts"""
@@ -16,3 +16,19 @@ class PostListViewset(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.U
     def perform_create(self, serializer):
         """create a new post"""
         serializer.save(author=self.request.user)
+
+class GroupListViewset(viewsets.GenericViewSet, mixins.CreateModelMixin, mixins.UpdateModelMixin, mixins.ListModelMixin, mixins.DestroyModelMixin, mixins.RetrieveModelMixin):
+    """Manage the groups"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes=(IsAuthenticated,)
+    queryset = Group.objects.all()
+    serializer_class = GroupListSerializer
+
+class GroupPostListViewset(viewsets.GenericViewSet, mixins.ListModelMixin):
+    """Get all the posts in a specific group"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes=(IsAuthenticated,)
+    serializer_class=PostSerializer
+
+    def get_queryset(self, *args, **kwargs):
+        return Post.objects.filter(group_id=self.kwargs['group_id'])
